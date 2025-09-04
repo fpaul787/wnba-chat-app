ec973060fba8092962dc823218b1c8d1f8e68053
Frantz Paul <fpaul787@gmail.com>
2025-09-03 22:38:06 -0400

    rag service with chat prompt

--- backend/services/rag_service.py
+++ backend/services/rag_service.py
@@ -1,14 +1,34 @@
+from langchain_core.prompts import ChatPromptTemplate
 
+from services.embedding_service import get_embedding_model
 
 class RagService:
     def __init__(self):
         self.vector_store = None
+        self.embedding_model = get_embedding_model()
+
+    def __create_rag_chain__(self, prompt: str):
+        pass
+
+    def __create_chat_model_prompt__(self):
+        template = """
+        Use the following pieces of context to answer the question at the end. 
+        Please follow the following rules:
+        1. If you don't know the answer, don't try to make up an answer. Just say "I can't answer that".
+
+        {context}
+
+        Question: {question}
+        """
+        prompt = ChatPromptTemplate.from_template(template)
+        return prompt
 
     def __generate_response__(self, query: str) -> dict:
-        # Here you would implement the logic to generate a response based on the query
+        prompt = self.__create_chat_model_prompt__(query)
+        rag_chain = self.__create_rag_chain__(prompt)
+        response = None
         return {
-            "answer": f"Response to '{query}'",
-            "source_documents": ["doc1", "doc2"]
+            "answer": response,
         }
 
     def query(self, query) -> dict:
