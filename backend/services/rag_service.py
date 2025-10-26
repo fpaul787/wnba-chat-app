9c1ad7a8692d2bc1cb30290501a3344cfda35cd5
Frantz Paul <fpaul787@gmail.com>
2025-10-26 19:33:16 -0400

    use embeddings service

--- backend/services/rag_service.py
+++ backend/services/rag_service.py
@@ -1,11 +1,12 @@
 from langchain_core.prompts import ChatPromptTemplate
 
-from services.embedding_service import get_embedding_model
+from services.embedding_service import get_hf_embedding_model
+from services.vector_store_service import get_vector_store
 
 class RagService:
     def __init__(self):
-        self.vector_store = None
-        self.embedding_model = get_embedding_model()
+        self.embedding_model = get_hf_embedding_model()
+        self.vector_store = get_vector_store("", self.embedding_model)
 
     def __create_rag_chain__(self, prompt: ChatPromptTemplate):
         pass
