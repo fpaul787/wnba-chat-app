1472a799a0a98b928498c9ebe5d7448571df3481
Frantz Paul <fpaul787@gmail.com>
2025-10-26 19:30:08 -0400

    new embedding service

--- backend/services/embedding_service.py
+++ backend/services/embedding_service.py
@@ -1,6 +1,21 @@
-from sentence_transformers import SentenceTransformer
+from langchain_huggingface import HuggingFaceEmbeddings
 
 model_name = 'sentence-transformers/all-MiniLM-L6-v2'
 
-def get_embedding_model():
-    return SentenceTransformer(model_name)
+def get_hf_embedding_model(cache_folder="./embeddings_cache"):
+    """
+    Get the Hugging Face embedding model.
+    """
+    embeddings = HuggingFaceEmbeddings(
+        model_name=get_embedding_model_name(),
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True},
+        cache_folder=cache_folder
+    )
+    return embeddings
+
+def get_embedding_model_name():
+    """
+    Get the name of the embedding model.
+    """
+    return model_name
