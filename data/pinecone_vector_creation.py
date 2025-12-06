a2fac21d81c297e5d0f50fea72937dd64e51fda6
fpaul787@gmail.com <fpaul787@gmail.com>
2025-12-06 02:34:54 +0000

    fix typo

--- data/pinecone_vector_creation.py
+++ data/pinecone_vector_creation.py
@@ -1,10 +1,14 @@
 # Databricks notebook source
 catalog = "frantzpaul_tech"
 schema = "wnba_chat"
-table_name = "new_articles"
+table_name = "news_articles"
 
 # COMMAND ----------
 
 data = spark.read.table(f"{catalog}.{schema}.{table_name}")
 display(data)
 data.count()
+
+# COMMAND ----------
+
+
