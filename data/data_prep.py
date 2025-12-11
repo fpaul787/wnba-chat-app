e2d345d93807e4b20b9a0003bc540a0a2185edf9
fpaul787@gmail.com <fpaul787@gmail.com>
2025-12-11 04:46:56 +0000

    refactoring

--- data/data_prep.py
+++ data/data_prep.py
@@ -15,7 +15,8 @@
 
 import json
 catalog = "frantzpaul_tech"
-schema = "wnba_rag"
+schema = "wnba_chat"
+table_name = "news_articles"
 
 # COMMAND ----------
 
@@ -24,18 +25,21 @@ schema = "wnba_rag"
 
 # COMMAND ----------
 
-path = "./wnba_news.json"
-with open(path, "r") as f:
-  data = json.load(f)
+# path = "./wnba_news.json"
+# with open(path, "r") as f:
+#   data = json.load(f)
+
+# # create dataframe
+# df = spark.createDataFrame(data)
+# display(df)
+
+# # save to table
+# # spark.createDataFrame(data).write.mode("overwrite").saveAsTable("wnba_news")
 
 # COMMAND ----------
 
-# create dataframe
-df = spark.createDataFrame(data)
-display(df)
-
-# save to table
-# spark.createDataFrame(data).write.mode("overwrite").saveAsTable("wnba_news")
+data = spark.read.table(f"{catalog}.{schema}.{table_name}")
+display(data)
 
 # COMMAND ----------
 
