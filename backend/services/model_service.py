d14fe8221c4f66c792efc563b78bf94b01e8f965
Frantz Paul <fpaul787@gmail.com>
2026-01-17 22:54:26 -0500

    using openai for model service

--- backend/services/model_service.py
+++ backend/services/model_service.py
@@ -1,8 +1,25 @@
-from langchain_openai import ChatOpenAI
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+from typing import List
+
 
 MODEL_NAME = "gpt-4o-mini"
-def get_current_chat_model():
+
+class ModelService:
     """
-    Get the current chat model instance.
+    Docstring for ModelService
     """
-    return ChatOpenAI(model=MODEL_NAME)
+    def __init__(self):
+        self.client = OpenAI(api_key="")
+        self.model_name = MODEL_NAME
+    
+    def query_model(self, messages: List[ChatCompletionMessageParam]):
+        """
+        Query the chat model with the given messages.
+        """
+        
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=messages
+        )
+        return response.choices[0].message.content
