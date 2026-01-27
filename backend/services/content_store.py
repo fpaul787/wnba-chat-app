50bb5d151d4154c35d60efb59c2cf8a1783f5ac0
Frantz Paul <fpaul787@gmail.com>
2026-01-26 23:43:51 -0500

    refactoring

--- backend/services/content_store.py
+++ backend/services/content_store.py
@@ -4,8 +4,11 @@ from models import Content
 from contextlib import contextmanager
 
 class ContentStoreService:
+    """
+    Service to interact with the content store that fetches text chunks.
+    """
     def __init__(self):
-        self.table_name = "frantzpaul_tech.wnba_chat.news_articles_chunked"
+        
 
     @contextmanager
     def _databricks_service(self):
@@ -23,8 +26,9 @@ class ContentStoreService:
         Fetch content chunks based on the given IDs.
         """
         id_list_str = ",".join([f"'{id_}'" for id_ in ids])
-        query = f"SELECT chunk_id, chunk_text FROM {self.table_name} WHERE chunk_id IN ({id_list_str})"
-        
+        table_name = "frantzpaul_tech.wnba_chat.news_articles_chunked"
+        query = f"SELECT chunk_id, chunk_text FROM {table_name} WHERE chunk_id IN ({id_list_str})"
+
         with self._databricks_service() as databricks_service:
             results = databricks_service.execute_query(query)
 
